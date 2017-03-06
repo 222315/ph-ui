b87ef88c26a1e20bd638bf0ea5ad232730821e55
Soumya <sbasak>
2017-03-06 01:15:17 -0700

    routing config has been committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import { CustomerDashboard }  from './customer/dashboard/dashboard.component';
 
+import { CustomerStatusComponent } from './vendor/customer-status.component';
+
+
+import { VendorDashboard } from './vendor/dashboard/dashboard.component'; //import home components
+
+
 const appRoutes: Routes = [
   /*{ path: 'customer', component: CustomerDashboard },*/
+  { path: 'vendor', component: VendorDashboard },
   { path: '',   redirectTo: '/customer', pathMatch: 'full' }
   /**{ path: 'home',        component: HomeComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
